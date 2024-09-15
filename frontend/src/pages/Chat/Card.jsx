b06f710e1fba3ab8caf5ed32243137093300dcb4
JoseDelacruzs <jose.delacruzsnt@uanl.edu.mx>
2024-09-15 10:03:28 -0600

    Rutas

--- frontend/src/pages/Chat/Card.jsx
+++ frontend/src/pages/Chat/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 // Componente para la Card
 const Card = ({ title, lastMessage, rating, timestamp }) => {
@@ -26,17 +27,24 @@ const Card = ({ title, lastMessage, rating, timestamp }) => {
     };
 
     return (
-        <div className="relative bg-primary text-white rounded-lg p-4 max-w-sm mx-auto mb-3">
-            {/* Rating en la esquina superior derecha */}
-            <div className="absolute top-2 right-2 flex items-center space-x-1">
-                <div className="flex">{renderStars()}</div>
-                <span className="ml-2 text-sm">{rating.toFixed(1)}</span>
-            </div>
-            {/* Contenido de la card */}
-            <h2 className="text-xl font-bold mb-2">{title}</h2>
-            <p className="text-base mb-2">{lastMessage}</p>
-            <span className="text-sm">{formatTimestamp(timestamp)}</span>
+        <div>
+            <Link to="/room">
+                <div>
+                    <div className="relative bg-primary text-white rounded-lg p-4 max-w-sm mx-auto mb-3">
+                        {/* Rating en la esquina superior derecha */}
+                        <div className="absolute top-2 right-2 flex items-center space-x-1">
+                            <div className="flex">{renderStars()}</div>
+                            <span className="ml-2 text-sm">{rating.toFixed(1)}</span>
+                        </div>
+                        {/* Contenido de la card */}
+                        <h2 className="text-xl font-bold mb-2">{title}</h2>
+                        <p className="text-base mb-2">{lastMessage}</p>
+                        <span className="text-sm">{formatTimestamp(timestamp)}</span>
+                    </div>
+                </div>
+            </Link>
         </div>
+
     );
 };
 
